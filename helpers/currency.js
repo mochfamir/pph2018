8104de2e406dd621621bdadc70d81563e2924597
Nsamudera <nsamudera12@gmail.com>
2018-10-25 22:18:11 +0700

    add currency helper

---
+++ helpers/currency.js
@@ -0,0 +1,15 @@
+function currency(number) {
+    let string = String(number)
+    let output = ''
+    let count = 0
+    for (let i = string.length - 1; i >= 0; i--) {
+        if (count === 3) {
+            output = string[i] + '.' + output
+            count = 0
+        } else {
+            output = string[i] + output
+        }
+        count += 1
+    }
+    return `Rp.${output},00`
+}
