d62219d6065ed533b8863d0b5b2ce9521c5d433d
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-10-25 17:03:30 +0700

    add app

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const app = express()
 const port = 3000
 const home = require('./routes/index.js')
 const admin = require('./routes/admin.js')
+const customer = require('./routes/customer.js')
 
 
 const userCustomer = require('./routes/customer_login.js')
 
 app.set('view engine', 'ejs')
+
 app.use(express.urlencoded({extended:false}))
 
 app.use('/', home)
 app.use('/admin', admin)
+app.use('/', customer)
 
 
 
