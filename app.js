7428b379f209c36cf51c3256b97b93e89579cbbb
Nsamudera <nsamudera12@gmail.com>
2018-10-26 10:00:53 +0700

    Heroku init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const home = require('./routes/index.js')
 const admin = require('./routes/admin.js')
 const logout = require('./routes/logout.js')
